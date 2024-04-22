7ca91aee181646d1a7a3260b9a64852a6a871791
stephon0518 <stephon2001@gmail.com>
2024-04-21 21:17:02 -0400

    Update app.py

--- app.py
+++ app.py
@@ -279,5 +279,5 @@ def register():
     
 if __name__ == '__main__':
     PORT = os.environ.get('PORT', 5000)
-    app.run(debug=False, host='0.0.0.0', port=PORT)
+    app.run(debug=True, host='0.0.0.0', port=PORT)
     #app.run(debug=True)
